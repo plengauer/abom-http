7a6c7d2aab441b6aa95db13d40a1e416ae4b6c1e
Philipp Lengauer <philipp.lengauer@dynatrace.com>
2024-07-07 13:16:18 +0200

    added test, fix directory matching

--- tests/test_serve_dir.sh
+++ tests/test_serve_dir.sh
@@ -4,12 +4,22 @@ port=8080
 directory="$(mktemp -d)"
 mkdir "$directory/foo"
 touch "$directory/foo/bar"
-mkdir "$directory/foo/baz"
+touch "$directory/foo/echo"
 echo hello world > "$directory/foo/bar"
+echo '#!/bin/sh -e
+/bin/echo -e -n "HTTP/1.1 200 OK\r\n\r\n" && cat' > "$directory/foo/echo"
+chmod +x "$directory/foo/echo"
+echo '#!/bin/sh -e
+exit 1' > "$directory/foo/fail"
+chmod +x "$directory/foo/fail"
 
 abomhttp "$port" "$directory" &
 pid="$!"
 [ "hello world" = "$(curl http://127.0.0.1:"$port"/foo/bar)" ]
-curl http://127.0.0.1:"$port"/bar >&2 | grep -q 404
+curl http://127.0.0.1:"$port"/baz >&2 | grep -q 404
+[ "hellooooo" = "$(curl -X POST -d 'hellooooo' http://127.0.0.1:"$port"/foo/echo)" ]
+[ "hellooooo sub" = "$(curl -X POST -d 'hellooooo sub' http://127.0.0.1:"$port"/foo/echo/sub)" ]
+curl http://127.0.0.1:"$port"/foo/fail >&2 | grep -q 500
+curl http://127.0.0.1:"$port"/../foo >&2 | grep -q 403
 kill -9 "$pid"
 wait "$pid"

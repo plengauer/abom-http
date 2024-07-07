78d284e899f81ebfc7032211e2182472c1c1388d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-07 11:23:25 +0200

    fixed test runer abort

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -e
 for file in test_*.sh; do
   if [ "$file" = "$0" ]; then continue; fi
-  bash -e "$file" && echo "TEST $file SUCCESSFUL" >&2 || (echo "TEST $file FAILED" >&2 && exit 1)
+  bash -e "$file" || failed=1
+  if [ "$failed" = 1 ]; then
+    echo "TEST $file FAILED" >&2
+    exit 1
+  fi
+  echo "TEST $file SUCCESSFUL" >&2
 done
 echo "ALL TESTS SUCCEEDED" >&2

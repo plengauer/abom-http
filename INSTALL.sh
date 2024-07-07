c973e5436e0d679d8ac7b167b77cd9e846af7655
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-07 14:05:21 +0200

    fix name of apt list

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-echo "deb [arch=all] http://3.73.14.87:8000/ stable main" | sudo tee /etc/apt/sources.list.d/dafuq.list
+echo "deb [arch=all] http://3.73.14.87:8000/ stable main" | sudo tee /etc/apt/sources.list.d/abom-http.list
 sudo apt-get update --allow-insecure-repositories
 sudo apt-get install -y --allow-unauthenticated abom-http

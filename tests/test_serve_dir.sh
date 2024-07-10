7b8c487000c733a887247f284e73313abeece6bb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2024-07-10 19:40:22 +0200

    fixed path

--- tests/test_serve_dir.sh
+++ tests/test_serve_dir.sh
@@ -24,7 +24,7 @@ printf 'GET /foo/fail HTTP/1.1\r\n\r\n' | netcat -w 3 127.0.0.1 "$port" | tee /d
 printf 'GET /../foo HTTP/1.1\r\n\r\n' | netcat -w 3 127.0.0.1 "$port" | tee /dev/stderr | grep -q 403
 printf 'GET /foo/test HTTP/1.1\r\n\r\n' | netcat -w 3 127.0.0.1 "$port" | tee /dev/stderr | grep -q 404
 printf 'PUT /foo/test HTTP/1.1\r\nContent-Length: 11\r\n\r\nhello world' | netcat -w 3 127.0.0.1 "$port" | tee /dev/stderr | grep -q 200
-[ "hello world" = "$(curl -v http://127.0.0.1:"$port"/test)" ]
+[ "hello world" = "$(curl -v http://127.0.0.1:"$port"/foo/test)" ]
 printf 'DELETE /foo/test HTTP/1.1\r\n\r\n' | netcat -w 3 127.0.0.1 "$port" | tee /dev/stderr | grep -q 200
 printf 'GET /foo/test HTTP/1.1\r\n\r\n' | netcat -w 3 127.0.0.1 "$port" | tee /dev/stderr | grep -q 404
 kill -9 "$pid"
